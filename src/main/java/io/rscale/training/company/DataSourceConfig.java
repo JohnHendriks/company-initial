17f9be72d5923e1b007c7966b69801aec2309cbd
John Hendriks <>
2017-10-19 11:17:31 +0200

    test

--- src/main/java/io/rscale/training/company/DataSourceConfig.java
+++ src/main/java/io/rscale/training/company/DataSourceConfig.java
@@ -1,60 +1,44 @@
 package io.rscale.training.company;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.UnsatisfiedDependencyException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.springframework.cloud.config.java.AbstractCloudConfig;
 
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.apache.log4j.Logger;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+
+@Profile("cloud")
 @Configuration
-public class DataSourceConfig {
+public class DataSourceConfig extends AbstractCloudConfig {
 
     private static final Logger logger = Logger.getLogger(DataSourceConfig.class);
 
-    @Value("${mysql.dbName}")
-    private String mysqlDbName;
-    
-    @Value("${mysql.user}")
-    private String mysqlUser;
-    
-    @Value("${mysql.password}")
-    private String mysqlPassword;
-    
-    @Value("${mysql.server}")
-    private String mysqlServer;
-    
-    @Value("${mysql.port}")
-    private int mysqlPort;
-    
-    private DataSource dataSource;
-    
-    public DataSourceConfig() {}
-    
-    @PostConstruct
-    public void init() {
+    public DataSourceConfig() {
         logger.info(this.getClass() + " loaded");
-        try {
-	    	MysqlDataSource mysql = new MysqlDataSource();
-	    	mysql.setDatabaseName(mysqlDbName);
-	    	mysql.setUser(mysqlUser);
-	    	mysql.setPassword(mysqlPassword);
-	    	mysql.setServerName(mysqlServer);
-	    	mysql.setPort(mysqlPort);
-	    	logger.info("Created datasource:  " + mysql);
-	        this.dataSource = mysql;
-    	} catch (Exception e) {
-    		logger.error("Could not create datasoruce", e);
-    	}
-        logger.info(this.getClass() + " initialized");
     }
 
     @Bean
-    public DataSource dataSource() {
-    	return dataSource;
+    public DataSource dataSource() throws Exception {
+        DataSource dataSource = cloud().getSingletonServiceConnector(DataSource.class, null);
+        if ( !isMySQL(dataSource)) {
+            logger.error("MySQL required when running cloud profile.");
+            throw new UnsatisfiedDependencyException("javax.sql.DataSource", "javax.sql.DataSource", "javax.sql.DataSource", "MySQL required when running cloud profile.");
+        }
+        return dataSource;
+    }
+
+    private boolean isMySQL(DataSource dataSource) throws SQLException {
+        try(Connection connection = dataSource.getConnection()){
+            return "MySQL".equalsIgnoreCase(connection.getMetaData().getDatabaseProductName());
+        }
     }
 
 }
--- src/main/java/io/rscale/training/company/SampleData.java
+++ src/main/java/io/rscale/training/company/SampleData.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("dev")
+@Profile("cloud")
 public class SampleData implements CommandLineRunner {
 
     private final CompanyRepository repository;
